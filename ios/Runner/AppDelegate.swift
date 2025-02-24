0149c68626bedfccf97ac95091c6386c03235a0a
Walid <walid_fekry@hotmail.com>
2025-02-24 02:48:15 +0200

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,70 +1,33 @@
 import UIKit
 import Flutter
-import Firebase
 import FirebaseMessaging
 import UserNotifications
 import flutter_local_notifications
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate{
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        // Initialize Firebase
-        FirebaseApp.configure()
-        // Initialize Messaging
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
-        application.registerForRemoteNotifications()
-        Messaging.messaging().delegate = self
-        GeneratedPluginRegistrant.register(with: self)
-        // Flutter local notification
-        FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-            GeneratedPluginRegistrant.register(with: registry)
+@main
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+          // Initialize Messaging
+          if #available(iOS 10.0, *) {
+              UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+              let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+              UNUserNotificationCenter.current().requestAuthorization(
+                  options: authOptions,
+                  completionHandler: {_, _ in })
+          } else {
+              let settings: UIUserNotificationSettings =
+                  UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+              application.registerUserNotificationSettings(settings)
           }
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    override func application(_ application: UIApplication,
-      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      // Set the APNs token for your device
-      Messaging.messaging().apnsToken = deviceToken
-      super.application(application,
-      didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-      }
-
-    @objc func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        // Handle FCM token
-        print("FCM token: \(fcmToken ?? "")")
-        // If you want to send the token to your server...
-    }
-    
-    // Handle incoming messages
-    override func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-    ) {
-        completionHandler([.alert, .sound, .badge])
-    }
-    
-    override func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        didReceive response: UNNotificationResponse,
-        withCompletionHandler completionHandler: @escaping () -> Void
-    ) {
-        let userInfo = response.notification.request.content.userInfo
-            NotificationCenter.default.post(name: NSNotification.Name("FCMNotificationReceived"), object: nil, userInfo: userInfo)
-        completionHandler()
-    }
+          application.registerForRemoteNotifications()
+          Messaging.messaging().delegate = self
+          GeneratedPluginRegistrant.register(with: self)
+          // Flutter local notification
+          FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+          GeneratedPluginRegistrant.register(with: registry)}
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
